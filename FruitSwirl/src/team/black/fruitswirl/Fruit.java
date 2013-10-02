60ef288a7ccc427815a289811b78e9c060693c4c
Alex Colson <guythundar@gmail.com>
2013-10-02 19:06:52 -0400

    Added more Fruit States and Flip constants.
    Added extra states to Fruit including STATE_FLIPPING. STATE_FLIPPING. If
    STATE_FLIPPING is true, then FLIP_* consts are used to determine flip 
    orientation.
    
    Signed-off-by: Alex Colson <guythundar@gmail.com>

--- FruitSwirl/src/team/black/fruitswirl/Fruit.java
+++ FruitSwirl/src/team/black/fruitswirl/Fruit.java
@@ -4,17 +4,38 @@ import org.flixel.FlxSprite;
 
 public class Fruit extends FlxSprite {
 	
-	public static final int STATE_NORMAL = 0x000;
-	public static final int STATE_FIERY = 0x001;
-	public static final int STATE_ELEC = 0x010;
-	public static final int STATE_LOCK = 0x100;
+	public static final int STATE_NONE		= 0x00000000;
+	public static final int STATE_NORMAL 	= 0x00000001;
+	public static final int STATE_FIRE 		= 0x00000010;
+	public static final int STATE_ELEC 		= 0x00000100;
+	public static final int STATE_LOCKING 	= 0x00001000;
+	public static final int STATE_LOCKED 	= 0x00010000;
+	public static final int STATE_DESTROY	= 0x00100000;
+	public static final int STATE_FALLING	= 0x01000000;
+	public static final int STATE_FLIPPING	= 0x10000000;
+	
+	public static final int FLIP_NONE	= 0x0000;
+	public static final int FLIP_LEFT	= 0x0001;
+	public static final int FLIP_RIGHT	= 0x0010;
+	public static final int FLIP_UP		= 0x0100;
+	public static final int FLIP_DOWN	= 0x1000;
+	
 	
 	private int curState;
+	private int flipDirection;
+	
+	public Fruit(float _ix, float _iy){
+		super(_ix, _iy);
+		curState = STATE_NONE;
+		flipDirection = FLIP_NONE;
+	}
 	
 	public Fruit(float _ix, float _iy, int _initState){
 		super(_ix, _iy);
 		curState = _initState;
+		flipDirection = FLIP_NONE;
 	}
+	
 	
 	@Override
 	public void update(){
