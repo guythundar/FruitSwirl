590fc2d537399d9e868f8b9a78b82522ac1ac9ca
Alex Colson <guythundar@gmail.com>
2013-10-15 20:48:14 -0400

    Added Grid Class.
    Grid is the drawable contianer for each Fruit piece in the game as well
    as the logic container. Most of the serious game logic will take place on
    the Grid.
    
    Signed-off-by: Alex Colson <guythundar@gmail.com>

---
+++ FruitSwirl/src/team/black/fruitswirl/Grid.java
@@ -0,0 +1,22 @@
+package team.black.fruitswirl;
+
+import org.flixel.FlxGroup;
+
+public class Grid extends FlxGroup {
+
+	public Grid() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public Grid(int MaxSize) {
+		super(MaxSize);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void update(){
+		
+	}
+	
+}
